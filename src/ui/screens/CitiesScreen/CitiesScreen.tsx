52a64a63fd7af72fa0bd8a1bd8f8789d1872ea8f
c.sepulveda.silva <sepulveda.cristobal.ignacio@gmail.com>
2025-02-17 12:02:01 -0300

    4th task done

--- src/ui/screens/CitiesScreen/CitiesScreen.tsx
+++ src/ui/screens/CitiesScreen/CitiesScreen.tsx
@@ -1,18 +1,71 @@
-import { View, FlatList, StyleSheet } from "react-native";
-import { citiesData } from "../../../utils/citiesData";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  FlatList,
+  StyleSheet,
+  ActivityIndicator,
+  TextInput,
+} from "react-native";
+import axios from "axios";
 import CityCard from "./components/CityCard";
+import City from "../../../utils/types"; // City type: { name: string; image: string; }
 
-export default function CitiesScreen() {
+interface ApiResponse {
+  data: City[];
+  status: number;
+  statusMsg: string;
+}
+
+const CitiesScreen: React.FC = () => {
+  const [cities, setCities] = useState<City[]>([]);
+  const [filteredCities, setFilteredCities] = useState<City[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [searchText, setSearchText] = useState<string>("");
+
+  useEffect(() => {
+    axios
+      .get<ApiResponse>("https://mytinerary-server.onrender.com/api/cities")
+      .then((response) => {
+        const fetchedCities = response.data.data;
+        setCities(fetchedCities);
+        setFilteredCities(fetchedCities);
+      })
+      .catch((error) => {
+        console.error("Error fetching cities:", error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  const handleSearch = (text: string) => {
+    setSearchText(text);
+    const filtered = cities.filter((city) =>
+      city.name.toLowerCase().includes(text.toLowerCase())
+    );
+    setFilteredCities(filtered);
+  };
+
   return (
     <View style={styles.container}>
-      <FlatList
-        data={citiesData}
-        keyExtractor={(item) => item.name}
-        renderItem={({ item }) => <CityCard city={item} />}
+      <TextInput
+        style={styles.searchInput}
+        placeholder="Busca una ciudad..."
+        value={searchText}
+        onChangeText={handleSearch}
       />
+      {loading ? (
+        <ActivityIndicator style={styles.loadingView} size="large" />
+      ) : (
+        <FlatList
+          data={filteredCities}
+          keyExtractor={(item, index) => `${item.name}-${index}`}
+          renderItem={({ item }) => <CityCard city={item} />}
+        />
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -20,4 +73,18 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     padding: 10,
   },
+  loadingView: {
+    color: "#0000ff",
+    marginTop: 20,
+  },
+  searchInput: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 8,
+    borderRadius: 5,
+  },
 });
+
+export default CitiesScreen;
